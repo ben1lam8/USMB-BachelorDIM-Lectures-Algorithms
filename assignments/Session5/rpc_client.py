5d4739f8752fe62279d001b78dfb93f1e21dc062
Benoit LAMIT <benoit.lamit@hotmail.fr>
2017-10-30 19:37:49 +0100

    Session5- Exercice1 refactored

---
+++ assignments/Session5/rpc_client.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+
+## @namespace S5_rpc_client
+# A client-side script that publish RPC requests
+#
+# @author Benoit Lamit, LPro DIM, IUT Annecy le vieux, FRANCE
+
+
+import pika;
+import os;
+import uuid;
+
+
+# Configure connection and message
+instance_provider = "CloudAMQP";
+instance_url = "amqp://doqhhehi:Ru34cC1OhqyM3xSItAJ7qHkui68sj4KM@lark.rmq.cloudamqp.com/doqhhehi";
+
+full_url = os.environ.get('CLOUDAMQP_URL', instance_url);
+
+params = pika.URLParameters(full_url);
+params.socket_timeout = 5;
+
+request_queue_name="rpc_queue"
+request_content = "Hi, how are you ?";
+
+corr_id = str(uuid.uuid4());
+
+# --- STEP 1 : EMIT A REQUEST
+
+# Connect to request queue
+connection = pika.BlockingConnection(params);
+channel = connection.channel();
+
+# Create an anonymous queue for the server response
+result = channel.queue_declare(exclusive=True);
+callback_queue = result.method.queue;
+
+# Publish on the request queue
+channel.basic_publish(exchange='',
+                      routing_key='rpc_queue',
+                      properties=pika.BasicProperties(
+                          reply_to=callback_queue,
+                          correlation_id=corr_id, ),
+                      body=request_content);
+
+# Report he request
+print("--- Request published ---\n");
+print("Destination Queue : {ip}.{q}".format(ip=instance_provider, q=request_queue_name));
+print("Request content : {b}".format(b=request_content));
+
+# --- STEP 2 : WAIT FOR A RESPONSE
+
+response_content = None;
+
+
+## The callback method, launched on every response consumption.
+#
+# @param ch the input channel
+# @param method
+# @param properties
+# @param body unneeded body of the request
+def on_response(ch, method, properties, body):
+    if corr_id == properties.correlation_id:
+
+        # Report the response
+        global response_content;
+        response_content = str(body);
+
+        print("--- Response received ---\n");
+        print("Source queue : {ip}.{q}".format(ip=instance_provider, q=callback_queue));
+        print("Response content : {b}".format(b=response_content));
+
+    else:
+        raise ValueError('Warning : the client received a corrupted response');
+
+
+# Define a consumer behaviour for the response queue (here : automatic message deletion)
+channel.basic_consume(on_response,
+                      queue=callback_queue,
+                      no_ack=True);
+
+# Wait for responses
+print('Wait for responses... ');
+
+channel.basic_consume(on_response,
+                      no_ack=True,
+                      queue=callback_queue);
+
+while response_content is None:
+    connection.process_data_events();
+
+# Disconnect
+connection.close();
---
+++ assignments/Session5/rpc_server.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+## @namespace S5_rpc_server
+# A server-side script that handles RPC requests
+#
+# @author Benoit Lamit, LPro DIM, IUT Annecy le vieux, FRANCE
+
+import pika;
+import os;
+
+
+# Configure connection
+instance_provider = "CloudAMQP";
+instance_url = "amqp://doqhhehi:Ru34cC1OhqyM3xSItAJ7qHkui68sj4KM@lark.rmq.cloudamqp.com/doqhhehi";
+
+full_url = os.environ.get('CLOUDAMQP_URL', instance_url);
+
+params = pika.URLParameters(full_url);
+params.socket_timeout = 5;
+
+queue_name = "rpc_queue";
+
+connection = pika.BlockingConnection(params);
+
+channel = connection.channel();
+channel.queue_declare(queue=queue_name);
+
+
+## The callback method, launched on every request consumption. Publishes a response.
+#
+# @param ch the input channel
+# @param method
+# @param properties
+# @param body unneeded body of the request
+def on_request(ch, method, properties, body):
+    print("Request Received. Responding...");
+    response = 'Fine and you ?';
+    ch.basic_publish(exchange='',
+                     routing_key=properties.reply_to,
+                     properties=pika.BasicProperties(
+                         correlation_id=properties.correlation_id),
+                     body=str(response));
+    ch.basic_ack(delivery_tag=method.delivery_tag); #Acknowledge
+    print("Response sent.");
+
+
+# Define a consumer behaviour (here : automatic message deletion)
+channel.basic_qos(prefetch_count=1);
+channel.basic_consume(on_request, queue=queue_name);
+
+# Wait for requests
+print('Wait for requests... ');
+channel.start_consuming();
